b8a06bd5ebf7ee0a216c908fca2aee260b3f93b1
Emimed04 <emimed.04@comunidad.unam.mx>
2022-03-02 12:27:33 -0600

    Initial Commit

---
+++ Cafeterias/CafeteriasApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeteriasApp.swift
+//  Cafeterias
+//
+//  Created by Emiliano  Medina Delgado  on 02/03/22.
+//
+
+import SwiftUI
+
+@main
+struct CafeteriasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cafeterias/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cafeterias
+//
+//  Created by Emiliano  Medina Delgado  on 02/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
